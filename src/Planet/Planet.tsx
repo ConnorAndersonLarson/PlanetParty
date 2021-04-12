1ae2386b89e405650c68156e4af59f25ca3ee68b
Alex Thompson <ahthomps1@gmail.com>
2021-04-11 18:53:51 -0600

    Add basic Planet facts to components return

--- src/Planet/Planet.tsx
+++ src/Planet/Planet.tsx
@@ -4,7 +4,20 @@ import './planet.css';
 const Planet = () => {
 
   return (
-
+    <section>
+      <h2>Earth</h2>
+      <img></img>
+      <article>
+        <h3>Planet Fun Facts:</h3>
+        <ul className='movie-info-list'>
+          <li><b>Mass:</b> </li>
+          <li><b>Distance from the sun:</b></li>
+          <li><b>Number of Moons:</b></li>
+          <li><b>Average Temperature:</b></li>
+          <li><b>Gravity:</b></li>
+        </ul>
+      </article>
+    </section>
   )
 }
 
