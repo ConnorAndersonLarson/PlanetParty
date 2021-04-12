0636ce895fc8cbf4e329596674dc1c8c8cffa094
Alex Thompson <ahthomps1@gmail.com>
2021-04-12 14:13:08 -0600

    Remove unnessary styles

--- src/Planet/Planet.tsx
+++ src/Planet/Planet.tsx
@@ -1,17 +1,15 @@
 import React from 'react';
 import './planet.css';
-// import earth from './planet-images/earth.svg';
 
-interface PlanetData {
+interface PlanetProps {
   name: string,
   id: number,
   planetFact: number
 }
 
 
-const Planet: React.FC<PlanetData> = ({ id, name, planetFact }): JSX.Element => {
+const Planet: React.FC<PlanetProps> = ({ id, name, planetFact }): JSX.Element => {
 
-  // const iconToDisplay = images.find(image => image.contains(name))
   return (
     <article className='planet-card' id={id.toString()}>
       <img className='planet-icon' alt='earth' src={`../planets/${name}.png`} />
