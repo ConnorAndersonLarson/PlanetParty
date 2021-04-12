30d86396c14dc4aef9e60cb22b166aa4543e03f0
Katie Bee <knees4bees@gmail.com>
2021-04-12 10:44:13 -0600

    Fix bug in radio buttons and add basic styling

--- src/components/SortBox/SortBox.tsx
+++ src/components/SortBox/SortBox.tsx
@@ -6,20 +6,20 @@ const SortBox: React.FC = () => {
   return (
     <div>
       <h2>Sort planets by:</h2>
+      <input type="radio" id="distance" name="sortCriteria" value="distance" defaultChecked></input>
       <label htmlFor="distance">Distance from sun</label>
-      <input type="radio" id="distance" name="distance" value="distance" checked></input>
+      <input type="radio" id="mass" name="sortCriteria" value="mass"></input>
       <label htmlFor="mass">Mass</label>
-      <input type="radio" id="mass" name="mass" value="mass"></input>
+      <input type="radio" id="diameter" name="sortCriteria" value="diameter"></input>
       <label htmlFor="diameter">Diameter</label>
-      <input type="radio" id="diameter" name="diameter" value="diameter"></input>
+      <input type="radio" id="gravity" name="sortCriteria" value="gravity"></input>
       <label htmlFor="gravity">Gravity</label>
-      <input type="radio" id="gravity" name="gravity" value="gravity"></input>
-      <label htmlFor="lengthOfDay">Length of Day</label>
-      <input type="radio" id="lengthOfDay" name="lengthOfDay" value="lengthOfDay"></input>
+      <input type="radio" id="lengthOfDay" name="sortCriteria" value="lengthOfDay"></input>
+      <label htmlFor="lengthOfDay">Length of day</label>
+      <input type="radio" id="meanTemperature" name="sortCriteria" value="meanTemperature"></input>
       <label htmlFor="meanTemperature">Average Temperature</label>
-      <input type="radio" id="meanTemperature" name="meanTemperature" value="meanTemperature"></input>
-      <label htmlFor="numberOfMoons">Number of Moons</label>
-      <input type="radio" id="numberOfMoons" name="numberOfMoons" value="numberOfMoons"></input>
+      <input type="radio" id="numberOfMoons" name="sortCriteria" value="numberOfMoons"></input>
+      <label htmlFor="numberOfMoons">Number of moons</label>
     </div>
   )
 }
