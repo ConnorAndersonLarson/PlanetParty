db762cb45bc921a8b39dee8d275566eaf01d1c0b
Alex Thompson <ahthomps1@gmail.com>
2021-04-12 10:40:21 -0600

    Add basic return structure to PlanetInfo component

--- src/PlanetInfo/PlanetInfo.tsx
+++ src/PlanetInfo/PlanetInfo.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
 import './planetInfo.css';
 
-const PlanetInfo = () => {
+const PlanetInfo: React.FC = () => {
 
   return (
-
+    <section className='planet-info-view'>
+      <h2>Earth</h2>
+      <img></img>
+      <article>
+        <h3>Planet Fun Facts:</h3>
+        <ul className='planet-info-list'>
+          <li><b>Mass:</b> </li>
+          <li><b>Distance from the sun:</b></li>
+          <li><b>Number of Moons:</b></li>
+          <li><b>Average Temperature:</b></li>
+          <li><b>Gravity:</b></li>
+        </ul>
+      </article>
+    </section>
   )
 }
 
