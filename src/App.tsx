4e2cf5f1a04247c55731dd716e70c559cd454403
Connor Anderson Larson <Connorandersonlarson@gmail.com>
2021-04-12 11:02:40 -0500

    Add sprites, text

--- src/App.tsx
+++ src/App.tsx
@@ -31,12 +31,16 @@ class App extends React.Component<{}, AllData> {
         <header className="App-header">
           <iframe src="https://giphy.com/embed/VI2UC13hwWin1MIfmi" className="giphy-embed"></iframe>
           <h1 className="title">Planet Party!</h1>
-          <img className="galaxy" src="galaxy2.png" alt="galaxy" />
         </header>
         <main>
+
           <h2>SortBox</h2>
           <h2>Planetarium</h2>
         </main>
+        <footer>
+          <p className="credits">Icons made by <a href="https://www.flaticon.com/authors/monkik" title="monkik">monkik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></p>
+          <p className="credits">Gif made by <a href="https://www.catchmaj.com/">Cat Chmaj</a> at <a href="https://giphy.com/gifs/VI2UC13hwWin1MIfmi">GIPHY</a></p>
+        </footer>
       </div>
     );
   }
