bdc85d4ef277198eb12225550dcac04be8d306ed
Alex Thompson <ahthomps1@gmail.com>
2021-04-14 10:43:00 -0600

    Add basic styling to sort options, remove radio button images

--- src/components/SortBox/SortBox.tsx
+++ src/components/SortBox/SortBox.tsx
@@ -7,23 +7,25 @@ interface Props {
 
 const SortBox: React.FC<Props> = ({ updateSort }) => {
   return (
-    <div>
+    <section className='sort-box'>
       <h2>Sort planets by:</h2>
-      <input type="radio" onClick={updateSort} id="distance" name="sortCriteria" value="distance_from_sun" defaultChecked></input>
-      <label htmlFor="distance">Distance from sun</label>
-      <input type="radio" onClick={updateSort} id="mass" name="sortCriteria" value="mass"></input>
-      <label htmlFor="mass">Mass</label>
-      <input type="radio" onClick={updateSort} id="diameter" name="sortCriteria" value="diameter"></input>
-      <label htmlFor="diameter">Diameter</label>
-      <input type="radio" onClick={updateSort} id="gravity" name="sortCriteria" value="gravity"></input>
-      <label htmlFor="gravity">Gravity</label>
-      <input type="radio" onClick={updateSort} id="lengthOfDay" name="sortCriteria" value="length_of_day"></input>
-      <label htmlFor="lengthOfDay">Length of day</label>
-      <input type="radio" onClick={updateSort} id="lengthOfYear" name="sortCriteria" value="length_of_year"></input>
-      <label htmlFor="lengthOfYear">Length of year</label>
-      <input type="radio" onClick={updateSort} id="numberOfMoons" name="sortCriteria" value="number_of_moons"></input>
-      <label htmlFor="numberOfMoons">Number of moons</label>
-    </div>
+      <div className='sort-options'>
+        <input type="radio" onClick={updateSort} id="distance" name="sortCriteria" value="distance_from_sun" defaultChecked></input>
+        <label htmlFor="distance">Distance from sun</label>
+        <input type="radio" onClick={updateSort} id="mass" name="sortCriteria" value="mass"></input>
+        <label htmlFor="mass">Mass</label>
+        <input type="radio" onClick={updateSort} id="diameter" name="sortCriteria" value="diameter"></input>
+        <label htmlFor="diameter">Diameter</label>
+        <input type="radio" onClick={updateSort} id="gravity" name="sortCriteria" value="gravity"></input>
+        <label htmlFor="gravity">Gravity</label>
+        <input type="radio" onClick={updateSort} id="lengthOfDay" name="sortCriteria" value="length_of_day"></input>
+        <label htmlFor="lengthOfDay">Length of day</label>
+        <input type="radio" onClick={updateSort} id="lengthOfYear" name="sortCriteria" value="length_of_year"></input>
+        <label htmlFor="lengthOfYear">Length of year</label>
+        <input type="radio" onClick={updateSort} id="numberOfMoons" name="sortCriteria" value="number_of_moons"></input>
+        <label htmlFor="numberOfMoons">Number of moons</label>
+      </div>
+    </section>
   )
 }
 
