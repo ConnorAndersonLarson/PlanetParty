826a1b4ec30e71b17729a033930fc248716041bd
Katie Bee <knees4bees@gmail.com>
2021-04-14 22:01:02 -0600

    Display all planet data in planet detail view

--- src/components/PlanetInfo/PlanetInfo.tsx
+++ src/components/PlanetInfo/PlanetInfo.tsx
@@ -5,22 +5,34 @@ import { PlanetBio } from '../../interface';
 interface InfoProps {
   currentPlanet: PlanetBio;
 }
+  // id: number,
+  // name: string,
+  // mass: number,
+  // diameter: number,
+  // gravity: number,
+  // length_of_day: number,
+  // distance_from_sun: number,
+  // length_of_year: number,
+  // number_of_moons: number,
 
 const PlanetInfo: React.FC<InfoProps> = ({ currentPlanet }): JSX.Element => {
 
-  const { name, mass, distance_from_sun, number_of_moons, length_of_year, gravity } = currentPlanet;
+  const {name, mass, diameter, gravity, length_of_day, distance_from_sun, length_of_year, number_of_moons } = currentPlanet;
+  // const { name, mass, distance_from_sun, number_of_moons, length_of_year, gravity } = currentPlanet;
   return (
     <section className='planet-info-view'>
       <h2>{name}</h2>
       <img className='planet-info-img' src={`../planets/${name}.png`}></img>
       <article>
-        <h3>Planet Fun Facts:</h3>
+        <h3>Fun Facts:</h3>
         <ul className='planet-info-list'>
-          <li><b>Mass: {mass}</b> </li>
           <li><b>Distance from the sun: {distance_from_sun}</b></li>
-          <li><b>Number of Moons: {number_of_moons}</b></li>
+          <li><b>Mass: {mass}</b> </li>
+          <li><b>Diameter: {diameter}</b> </li>
+          <li><b>Gravity: {gravity}</b> </li>
+          <li><b>Length of day: {length_of_day}</b></li>
           <li><b>Length of year: {length_of_year}</b></li>
-          <li><b>Gravity: {gravity}</b></li>
+          <li><b>Number of moons: {number_of_moons}</b></li>
         </ul>
       </article>
     </section>
