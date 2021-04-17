eebb97acc3caf94cb048017d83cd4c6d769a6dd0
Katie Bee <knees4bees@gmail.com>
2021-04-16 23:39:55 -0600

    Create draft icon for length of day

--- src/components/SortBox/SortBox.tsx
+++ src/components/SortBox/SortBox.tsx
@@ -19,7 +19,7 @@ const SortBox: React.FC<Props> = ({ updateSort }) => {
         <input type="radio" onClick={updateSort} id="gravity" name="sortCriteria" value="gravity"></input>
         <label htmlFor="gravity"><img className='sort-icon' alt='astronaut icon' src='../space/falling-star.png'></img>Gravity</label>
         <input type="radio" onClick={updateSort} id="lengthOfDay" name="sortCriteria" value="length_of_day"></input>
-        <label htmlFor="lengthOfDay"><img className='sort-icon' alt='astronaut icon' src='../space/orbit.png'></img>Length of day</label>
+        <label htmlFor="lengthOfDay"><img className='sort-icon' alt='astronaut icon' src='../space/planet-axis.svg'></img>Length of day</label>
         <input type="radio" onClick={updateSort} id="lengthOfYear" name="sortCriteria" value="length_of_year"></input>
         <label htmlFor="lengthOfYear"><img className='sort-icon' alt='astronaut icon' src='../space/solar-system.png'></img>Length of year</label>
         <input type="radio" onClick={updateSort} id="numberOfMoons" name="sortCriteria" value="number_of_moons"></input>
