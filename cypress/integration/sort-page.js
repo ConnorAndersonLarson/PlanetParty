4822c023456b14b53ed54fc7db5d487934c8226d
Connor Anderson Larson <Connorandersonlarson@gmail.com>
2021-04-17 14:44:56 -0500

    Fix intercepts to listen for correct url

--- cypress/integration/sort-page.js
+++ cypress/integration/sort-page.js
@@ -4,7 +4,7 @@ describe('Sort Testing', () => {
   beforeEach(() => {
     cy.fixture('testPlanets.json')
       .then(planets => {
-        cy.intercept('https://dry-plains-91502.herokuapp.com/planets', {
+        cy.intercept('https://api.le-systeme-solaire.net/rest.php/bodies?data=%20id%2C%20englishName%2C%20moons%2C%20mass%2C%20gravity%2C%20massValue%2C%20massExponent%2C%20moon%2C%20meanRadius%2C%20sideralOrbit%2C%20sideralRotation%2C%20semimajorAxis&filter%5B%5D=isPlanet%2Cneq%2Ctrue&filter%5B%5D=', {
           body: planets
         })
       });
