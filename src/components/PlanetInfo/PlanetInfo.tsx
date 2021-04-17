c46a5a3ecf33027dd3ae3997310c5981f42c81e7
Katie Bee <knees4bees@gmail.com>
2021-04-17 16:21:57 -0600

    Set up basic info grid structure

--- src/components/PlanetInfo/PlanetInfo.tsx
+++ src/components/PlanetInfo/PlanetInfo.tsx
@@ -7,6 +7,10 @@ interface InfoProps {
   currentPlanet: PlanetBio;
 }
 
+const getMoonWord = (moonCount: number) => {
+  return moonCount === 1 ? 'moon' : 'moons';
+}
+
 const PlanetInfo: React.FC<InfoProps> = ({ currentPlanet }): JSX.Element => {
 
   const { name, mass, diameter, gravity, length_of_day, distance_from_sun, length_of_year, number_of_moons } = currentPlanet;
@@ -14,20 +18,48 @@ const PlanetInfo: React.FC<InfoProps> = ({ currentPlanet }): JSX.Element => {
   return (
     <section className='planet-info-view'>
       <Link to="/" className='back'>⬅ Back to all planets</Link>
-      <h2 className='planet-info-title' >{name}</h2>
-      <img className='planet-info-img' src={`../planet-pics/${name}-pic.jpg`} alt={name}></img>
-      <article className='planet-info-box'>
-        <h3 className='fun-fact-header'>{name} Fun Facts</h3>
-        <ul className='planet-info-list'>
-          <li><b>Distance from the sun:</b> {distance_from_sun} km</li>
-          <li><b>Mass:</b> {mass} kg</li>
-          <li><b>Diameter:</b> {diameter} km</li>
-          <li><b>Gravity:</b> {gravity} m/s²</li>
-          <li><b>Length of day:</b> {length_of_day} hours</li>
-          <li><b>Length of year:</b> {length_of_year} days</li>
-          <li><b>Number of moons:</b> {number_of_moons}</li>
-        </ul>
-      </article>
+      <div className='planet-info-container'>
+        <div className='column'>
+          <div className='planet-info-card planet-info-card__name'>
+            <h2 className='planet-info-title' >{name}</h2>
+          </div>
+          <div className='planet-info-card planet-info-card__image'>
+            <img className='planet-info-img' src={`../planet-pics/${name}-pic.jpg`} alt={name}></img>
+          </div>
+          <div className='planet-info-card planet-info-card__mass'>
+            <h2 className='planet-info-mass-title' >Mass</h2>
+            <h3 className='planet-info-mass' >{mass} kg</h3>
+          </div>
+        </div>
+        <div className='column'>
+          <div className='planet-info-card planet-info-card__year'>
+            <h2 className='planet-info-length-of-year__title' >Length of year</h2>
+            <h3 className='planet-info-length-of-year' >{length_of_year} days</h3>
+          </div>
+          <div className='planet-info-card planet-info-card__gravity'>
+            <h2 className='planet-info-gravity__title' >Gravity</h2>
+            <h3 className='planet-info-gravity' >{gravity} m/s²</h3>
+          </div>
+          <div className='planet-info-card planet-info-card__diameter'>
+            <h2 className='planet-info-diameter__title' >Diameter</h2>
+            <h3 className='planet-info-diameter' >{diameter} km</h3>
+          </div>
+        </div>
+        <div className='column'>
+          <div className='planet-info-card planet-info-card__distance'>
+            <h2 className='planet-info-distance-from-sun__title' >Distance from sun</h2>
+            <h3 className='planet-info-distance-from-sun' >{distance_from_sun} km</h3>
+          </div>
+          <div className='planet-info-card planet-info-card__moons'>
+            <h2 className='planet-info-moons__title' >Number of moons</h2>
+            <h3 className='planet-info-moons' >{number_of_moons} {getMoonWord(number_of_moons)}</h3>
+          </div>
+          <div className='planet-info-card planet-info-card__day'>
+            <h2 className='planet-info-length-of-day__title' >Length of day</h2>
+            <h3 className='planet-info-length-of-day' >{length_of_day} hours</h3>
+          </div>
+        </div>
+      </div>
     </section>
   )
 }
