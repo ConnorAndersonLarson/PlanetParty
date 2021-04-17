7472b14e8a15c1ba001427494ec5c6d8fefe4fed
Connor Anderson Larson <Connorandersonlarson@gmail.com>
2021-04-16 21:56:56 -0500

    Fix bug where alt image text was all earth

--- src/components/Planet/Planet.tsx
+++ src/components/Planet/Planet.tsx
@@ -12,7 +12,7 @@ const Planet: React.FC<PlanetProps> = ({ id, name, planetFact }): JSX.Element =>
 
   return (
     <Link to={`/${name.toLowerCase()}`} className='planet-card' id={id.toString()}>
-      <img className='planet-icon' alt='earth' src={`../planets/${name}.png`} />
+      <img className='planet-icon' alt={name} src={`../planets/${name}.png`} />
       <h2 className='planet-card-name'>{name}</h2>
       <p className='planet-card-fact'>{planetFact}</p>
     </Link>
