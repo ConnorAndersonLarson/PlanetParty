9114eb0b23edacf4e57550ab310dc979b12b5ea3
Alex Thompson <ahthomps1@gmail.com>
2021-04-17 16:35:42 -0600

    Create editable input component

---
+++ src/components/Edit/Edit.tsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+
+const Edit: React.FC<any> = (): JSX.Element => {
+  const [isInputActive, setIsInputActive] = useState(false);
+  const [input, setInput] = useState('');
+
+  return (
+    <input />
+  )
+}
+
+export default Edit;
