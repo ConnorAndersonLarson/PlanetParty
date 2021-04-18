4c6da5ba7a37350c07875cf47effc56854c27700
Connor Anderson Larson <Connorandersonlarson@gmail.com>
2021-04-17 19:30:28 -0500

    Add tests for single planet pages

--- cypress/integration/api-test.js
+++ cypress/integration/api-test.js
@@ -36,6 +36,21 @@ it('Should show an error message on failed load', () => {
       cy.get('.App').contains('Oh no! Something went wrong with the data launch!')
     })
 
+  it('Should show an error message when planet data is unavailable', () => {
+      cy.intercept({
+        url: 'https://api.le-systeme-solaire.net/rest.php/bodies?data=%20id%2C%20englishName%2C%20moons%2C%20mass%2C%20gravity%2C%20massValue%2C%20massExponent%2C%20moon%2C%20meanRadius%2C%20sideralOrbit%2C%20sideralRotation%2C%20semimajorAxis&filter%5B%5D=isPlanet%2Cneq%2Ctrue&filter%5B%5D='
+      }, {
+        statusCode: 500,
+        body: {
+          "error": "Server Not Found"
+        }
+      })
+      cy.visit(`${baseURL}mercury`)
+      cy.get('.App').contains('Oops, looks like that planet is out of our solar system')
+    })
 
-
+    it('Should show an error message when an improper planet name is typed in the URL', () => {
+      cy.visit(`${baseURL}astroworld`)
+      cy.get('.App').contains('Oops, looks like that planet is out of our solar system')
+    })
 })
