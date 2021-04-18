703a91f7156f775e9c6b15e74035e9511d93b953
Katie Bee <knees4bees@gmail.com>
2021-04-18 11:31:45 -0600

    Style length of year section

--- src/components/PlanetInfo/PlanetInfo.tsx
+++ src/components/PlanetInfo/PlanetInfo.tsx
@@ -37,22 +37,25 @@ const PlanetInfo: React.FC<InfoProps> = ({ currentPlanet }): JSX.Element => {
         </div>
         <div className='planet-info-column planet-info-column-2'>
           <div className='planet-info-card planet-info-card__year'>
-            <img className='info-icon info-icon__lengthOfYear' alt='solar system icon' src='../space/solar-system-fill.svg'></img>
-            <h2 className='planet-info-title planet-info-title__length-of-year'>Length of year</h2>
-            <h3 className='planet-info-length-of-year'>{length_of_year} days</h3>
+            <img className='info-icon info-icon__length-of-year' alt='solar system icon' src='../space/solar-system-fill.svg'></img>
+            {name === 'Earth' && <h2 className='planet-info-title planet-info-title__length-of-year'>Length of year</h2>}
+            {name !== 'Earth' && <p className='planet-info-text planet-info-text__length-of-year'>If you lived on {name}, you'd {(name === 'Mercury' || name === 'Venus') && 'only'} have to wait <span className='length-of-year-multiplier'>{(length_of_year / 365).toFixed(2)}</span> <span className='length-of-year-words'>times as long </span> for your next birthday</p>}
+            {name === 'Earth' && <p className='planet-info-text planet-info-text__length-of-year'><span className='length-of-year-earth'>{length_of_year} days</span></p>}
           </div>
           <div className='planet-info-card planet-info-card__gravity'>
             <h2 className='planet-info-title planet-info-title__gravity'>Gravity</h2>
             <img className='info-icon info-icon__gravity' alt='comet icon' src='../space/comet-fill.svg'></img>
             <h3 className='planet-info-gravity'>{gravity} m/s²</h3>
-          <b>Gravity:</b><input
-            className='gravity-input'
-            value={input}
-            onChange={event => setInput(event.target.value)}
-            min='0'
-            type='number'
-            aria-label='Weight on Earth'
-          /> lbs on earth would weigh {input && gravityConversion(gravity)}{!input && gravityConversion(input)} lbs on {name}
+            Something that weighs
+              <input
+                className='gravity-input'
+                value={input}
+                onChange={event => setInput(event.target.value)}
+                min='0'
+                type='number'
+                aria-label='Weight on Earth'
+              /> 
+            lbs on earth would weigh {input && gravityConversion(gravity)}{!input && gravityConversion(input)} lbs on {name}
           </div>
           <div className='planet-info-card planet-info-card__diameter'>
             <img className='info-icon info-icon__diameter' alt='astronaut on orb icon' src='../space/visitor-fill.svg'></img>
@@ -71,7 +74,7 @@ const PlanetInfo: React.FC<InfoProps> = ({ currentPlanet }): JSX.Element => {
             <p className='planet-info-text planet-info-text__moons'>On {name} you {name !== 'Earth' && 'would'} see <span className='moon-count'>{number_of_moons}</span> <span className='moon-word'>{getMoonWord(number_of_moons)}</span> in the sky</p>
           </div>
           <div className='planet-info-card planet-info-card__day'>
-            <img className='info-icon info-icon__lengthOfDay' alt='spinning planet icon' src='../space/orbit-fill.svg'></img>
+            <img className='info-icon info-icon__length-of-day' alt='spinning planet icon' src='../space/orbit-fill.svg'></img>
             <h2 className='planet-info-title planet-info-title__length-of-day'>Length of day</h2>
             <h3 className='planet-info-length-of-day'>{length_of_day} hours</h3>
           </div>
