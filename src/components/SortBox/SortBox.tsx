f548b64c6a59d4e36ec6345ecf9e48a09dbeec59
Connor Anderson Larson <Connorandersonlarson@gmail.com>
2021-04-17 19:54:23 -0500

    Update alt text for sort icons

--- src/components/SortBox/SortBox.tsx
+++ src/components/SortBox/SortBox.tsx
@@ -8,19 +8,19 @@ const SortBox: React.FC<Props> = ({ updateSort }) => {
       <h2>Sort planets by:</h2>
       <div className='sort-options'>
         <input type="radio" onClick={updateSort} id="distance" name="sortCriteria" value="distance_from_sun"></input>
-        <label htmlFor="distance"><img className='sort-icon' alt='astronaut icon' src='../space/sun.png'></img>Distance from sun</label>
+        <label htmlFor="distance"><img className='sort-icon' alt='sun icon' src='../space/sun.png'></img>Distance from sun</label>
         <input type="radio" onClick={updateSort} id="mass" name="sortCriteria" value="mass"></input>
-        <label htmlFor="mass"><img className='sort-icon' alt='astronaut icon' src='../space/asteroid.png'></img>Mass</label>
+        <label htmlFor="mass"><img className='sort-icon' alt='astroid icon' src='../space/asteroid.png'></img>Mass</label>
         <input type="radio" onClick={updateSort} id="diameter" name="sortCriteria" value="diameter"></input>
         <label htmlFor="diameter"><img className='sort-icon' alt='astronaut icon' src='../space/visiter.png'></img>Diameter</label>
         <input type="radio" onClick={updateSort} id="gravity" name="sortCriteria" value="gravity"></input>
-        <label htmlFor="gravity"><img className='sort-icon' alt='astronaut icon' src='../space/comet.png'></img>Gravity</label>
+        <label htmlFor="gravity"><img className='sort-icon' alt='comet icon' src='../space/comet.png'></img>Gravity</label>
         <input type="radio" onClick={updateSort} id="lengthOfDay" name="sortCriteria" value="length_of_day"></input>
-        <label htmlFor="lengthOfDay"><img className='sort-icon' alt='astronaut icon' src='../space/orbit.png'></img>Length of day</label>
+        <label htmlFor="lengthOfDay"><img className='sort-icon' alt='orbit icon' src='../space/orbit.png'></img>Length of day</label>
         <input type="radio" onClick={updateSort} id="lengthOfYear" name="sortCriteria" value="length_of_year"></input>
-        <label htmlFor="lengthOfYear"><img className='sort-icon' alt='astronaut icon' src='../space/solar-system.png'></img>Length of year</label>
+        <label htmlFor="lengthOfYear"><img className='sort-icon' alt='solar system icon' src='../space/solar-system.png'></img>Length of year</label>
         <input type="radio" onClick={updateSort} id="numberOfMoons" name="sortCriteria" value="number_of_moons"></input>
-        <label htmlFor="numberOfMoons"><img className='sort-icon' alt='astronaut icon' src='../space/eclipse.png'></img>Number of moons</label>
+        <label htmlFor="numberOfMoons"><img className='sort-icon' alt='eclipse icon' src='../space/eclipse.png'></img>Number of moons</label>
       </div>
     </section>
   )
