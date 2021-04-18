c61850315d6ab0dd96fb5e704b689ebcb468f4c8
Alex Thompson <ahthomps1@gmail.com>
2021-04-18 11:58:05 -0600

    Add function to add commas to longer data in Planetarium component

--- src/components/Planetarium/Planetarium.tsx
+++ src/components/Planetarium/Planetarium.tsx
@@ -5,22 +5,36 @@ import Planet from '../Planet/Planet';
 
 const Planetarium: React.FC<AllData> = ({ allPlanets, sortKey }): JSX.Element => {
 
-  const addDescriptor = (data: number, key: string) => {
+  const formateLargeNumbers = (number: number): string => {
+    let totalFormatted = number.toString().split('.');
+    totalFormatted[0] = totalFormatted[0].replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+    console.log(totalFormatted[0])
+    return (totalFormatted.join('.'));
+  }
+
+  const addDescriptor = (data: number, key: string): string => {
     let descriptor: string = '';
+    let formattedData: string = ''
     if (key === 'distance_from_sun' || key === 'diameter') {
       descriptor = 'km';
+      formattedData = formateLargeNumbers(data);
     } else if (key === 'mass') {
       descriptor = 'quintillion kg';
+      formattedData = formateLargeNumbers(data);
     } else if (key === 'length_of_day') {
       descriptor = 'hours';
+      formattedData = `${data}`;
     } else if (key === 'length_of_year') {
       descriptor = 'days'
+      formattedData = `${data}`;
     } else if (key === 'number_of_moons') {
       descriptor = 'moons'
+      formattedData = `${data}`;
     } else if (key === 'gravity') {
       descriptor = 'm/s²'
+      formattedData = `${data}`;
     }
-    return (`${data} ${descriptor}`)
+    return (`${formattedData} ${descriptor}`)
   }
 
   const planetCards = allPlanets.map(planet => {
