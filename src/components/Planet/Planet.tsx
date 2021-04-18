a5dbeb5e8567397ba96af3b6e052600b445a0dd6
Connor Anderson Larson <Connorandersonlarson@gmail.com>
2021-04-18 13:02:50 -0500

    Add planet size increase on hover

--- src/components/Planet/Planet.tsx
+++ src/components/Planet/Planet.tsx
@@ -8,8 +8,8 @@ const Planet: React.FC<PlanetProps> = ({ id, name, planetFact }): JSX.Element =>
   return (
     <Link to={`/${name.toLowerCase()}`} className='planet-card' id={id.toString()}>
       <img className='planet-icon' alt={name} src={`../planets/${name}.png`} />
-      <h2 className='planet-card-name'>{name}</h2>
-      <p className='planet-card-fact'>{planetFact}</p>
+      <h2 className={`planet-card-name ${name}`}>{name}</h2>
+      <p className={`planet-card-fact ${name}`}>{planetFact}</p>
     </Link>
   )
 }
